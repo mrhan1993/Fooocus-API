5b0d6096a4651084a88a356974f1244b486dc709
Konie <guofengzuo@hotmail.com>
2023-11-22 14:14:42 +0800

    Merge for Fooocus v2.1.823

--- fooocusapi/repositories_versions.py
+++ fooocusapi/repositories_versions.py
@@ -1,5 +1,5 @@
 import os
 
-fooocus_version = '2.1.822'
+fooocus_version = '2.1.823'
 fooocus_commit_hash = os.environ.get(
-    'FOOOCUS_COMMIT_HASH', "dececbd06000304ef9cd6a43fcd0e685eab147e9")
+    'FOOOCUS_COMMIT_HASH', "bd4d40203c7e727fd06fc58ec122c25befcc5644")
