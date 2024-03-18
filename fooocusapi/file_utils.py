2a02b7043820df6d85ec67d67c232c63a72bab5f
mrhan1993 <50648276+mrhan1993@users.noreply.github.com>
2024-03-18 10:51:00 +0800

    support save extension

--- fooocusapi/file_utils.py
+++ fooocusapi/file_utils.py
@@ -5,6 +5,11 @@ import os
 import numpy as np
 from PIL import Image
 import uuid
+import json
+from pathlib import Path
+from PIL.PngImagePlugin import PngInfo
+
+
 
 output_dir = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..', 'outputs', 'files'))
@@ -13,16 +18,41 @@ os.makedirs(output_dir, exist_ok=True)
 static_serve_base_url = 'http://127.0.0.1:8888/files/'
 
 
-def save_output_file(img: np.ndarray) -> str:
+def save_output_file(img: np.ndarray, image_meta: dict = None,
+                     image_name: str = '', extension: str = 'png') -> str:
+    """
+    Save np image to file
+    Args:
+        img: np.ndarray image to save
+        image_meta: dict of image metadata
+        image_name: str of image name
+        extension: str of image extension
+    Returns:
+        str of file name
+    """
     current_time = datetime.datetime.now()
     date_string = current_time.strftime("%Y-%m-%d")
 
-    filename = os.path.join(date_string, str(uuid.uuid4()) + '.png')
+    image_name = str(uuid.uuid4()) if image_name == '' else image_name
+
+    filename = os.path.join(date_string, image_name + '.' + extension)
     file_path = os.path.join(output_dir, filename)
 
+    if extension not in ['png', 'jpg', 'webp']:
+        extension = 'png'
+
+    if image_meta is None:
+        image_meta = {}
+
+    meta = None
+    if extension == 'png':
+        meta = PngInfo()
+        meta.add_text("params", json.dumps(image_meta))
+
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    Image.fromarray(img).save(file_path)
-    return filename
+    Image.fromarray(img).save(file_path, format=extension,
+                              pnginfo=meta, optimize=True)
+    return Path(filename).as_posix()
 
 
 def delete_output_file(filename: str):
