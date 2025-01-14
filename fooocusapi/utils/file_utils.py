07b41ada3f5334e814df435319254f7578825012
Ninoambara <nino@alterolab.com>
2025-01-14 13:58:03 +0800

    feat: upload to pinita

--- fooocusapi/utils/file_utils.py
+++ fooocusapi/utils/file_utils.py
@@ -18,8 +18,8 @@ import numpy as np
 from PIL import Image
 from minio import Minio
 import requests
-
-
+import json
+from datetime import datetime
 from fooocusapi.utils.logger import logger
 
 
@@ -28,12 +28,15 @@ output_dir = os.path.abspath(os.path.join(
 os.makedirs(output_dir, exist_ok=True)
 
 STATIC_SERVER_BASE = 'http://127.0.0.1:3002/files/'
-API_KEY = "6c8b6b3e68c3a52eff8d"
-API_SECRET = "71dd146ef4b6fc8b642093d4b187225db9a513eca8a4f143c151e7026f9c50e9"
+API_KEY = os.environ.get('API_KEY')
+API_SECRET = os.environ.get('API_SECRET')
+MINIO_ACCESS_KEY = os.environ.get('MINIO_ACCESS_KEY')
+MINIO_SECRET_KEY = os.environ.get('MINIO_SECRET_KEY')
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 
 client = Minio("minio-ew8sowww4gsogokg8okw00sk.154.26.133.70.sslip.io",
-    access_key="s3bTdpCkT3IqVGlB",
-    secret_key="FyJXS9xiDAMbBBFQjryLuQrDn14ROLBE",
+    access_key=MINIO_ACCESS_KEY,
+    secret_key=MINIO_SECRET_KEY,
 )
 
 
@@ -50,10 +53,11 @@ def save_output_file(
     Returns:
         str of file name
     """
-    current_time = datetime.datetime.now()
+    current_time = datetime.now()
     date_string = current_time.strftime("%Y-%m-%d")
-    bucket_name =  "otti-image-generation"
-    image_data = narray_to_bytes(img) 
+    bucket_name = "otti-image-generation"
+
+    # Check if bucket exists
     found = client.bucket_exists(bucket_name)
     if not found:
         client.make_bucket(bucket_name)
@@ -61,27 +65,43 @@ def save_output_file(
     else:
         print("Bucket", bucket_name, "already exists")
 
-        
-        
+    # Determine file path
     filename = os.path.join(date_string, image_name + '.' + extension)
     file_path = os.path.join(output_dir, filename)
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
     try:
         if isinstance(img, str):
-            # shutil.move(img, file_path)
-         client.fput_object(
-            bucket_name, file_path, img,
-        )
-        upload_image_data_to_pinata(image_data)
+            # Move file to output directory
+            shutil.move(img, file_path)
+            # Upload to Minio
+            client.fput_object(bucket_name, filename, file_path)
+        else:
+            # Convert np.ndarray to bytes
+            image_data = narray_to_bytes(img)
+            # Save image data to file
+            with open(file_path, 'wb') as f:
+                f.write(image_data)
+            # Upload to Minio
+            client.put_object(bucket_name, filename, BytesIO(image_data), length=len(image_data))
+
+        # Upload to Pinata
+        pinita_hash = upload_public_file_to_pinata(file_path, filename)
+
         print(
             file_path, "successfully uploaded as object",
             filename, "to bucket", bucket_name,
         )
-        return Path(file_path).as_posix()
-    except Exception:
-        raise Exception
 
+         # Delete the local file after upload
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print(f"Local file {file_path} deleted")
+        
+        return pinita_hash
+    except Exception as e:
+        print("Error:", e)
+        raise Exception
 
 def delete_output_file(filename: str):
     """
@@ -101,28 +121,44 @@ def delete_output_file(filename: str):
         return False
 
 
-def output_file_to_base64img(filename: str | None) -> str | None:
+def output_file_to_base64img(url: str) -> str | None:
     """
-    Convert an image file to a base64 string.
+    Convert an image from a Pinata (IPFS) URL to a Base64 string.
+
     Args:
-        filename: str of file name
-    return: str of base64 string
+        url: The URL of the image on Pinata (IPFS).
+    Returns:
+        A Base64 string representation of the image or None if there's an error.
     """
-    if filename is None:
-        return None
-    file_path = os.path.join(output_dir, filename)
-    if not os.path.exists(file_path) or not os.path.isfile(file_path):
+    if not url:
         return None
 
-    ext = filename.split('.')[-1]
-    if ext.lower() not in ['png', 'jpg', 'webp', 'jpeg']:
-        ext = 'png'
-    img = Image.open(file_path)
-    output_buffer = BytesIO()
-    img.save(output_buffer, format=ext.upper())
-    byte_data = output_buffer.getvalue()
-    base64_str = base64.b64encode(byte_data).decode('utf-8')
-    return f"data:image/{ext};base64," + base64_str
+    try:
+        # Fetch the image from Pinata (IPFS)
+        response = requests.get(f"https://lime-absent-tarsier-87.mypinata.cloud/ipfs/{url}" , stream=True)
+        if response.status_code != 200:
+            return None
+
+        # Read the image content
+        img = Image.open(BytesIO(response.content))
+        
+        # Determine the file format and convert to Base64
+        ext = img.format.lower()  # Extract the format (e.g., PNG, JPEG)
+        if ext not in ['png', 'jpg', 'jpeg', 'webp']:
+            ext = 'png'  # Default to PNG if unsupported format
+        
+        # Save the image to a buffer
+        output_buffer = BytesIO()
+        img.save(output_buffer, format=ext.upper())
+        byte_data = output_buffer.getvalue()
+        
+        # Encode the image to Base64
+        base64_str = base64.b64encode(byte_data).decode('utf-8')
+        return f"data:image/{ext};base64," + base64_str
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 
 def output_file_to_bytesimg(filename: str | None) -> bytes | None:
@@ -154,45 +190,27 @@ def get_file_serve_url(filename: str | None) -> str | None:
     """
     if filename is None:
         return None
-    return "https://console-ew8sowww4gsogokg8okw00sk.154.26.133.70.sslip.io/otti-image-generation/files" + '/'.join(filename.split('/')[-2:])
+    return "https://lime-absent-tarsier-87.mypinata.cloud/ipfs/" + filename
 
-
-def upload_image_data_to_pinata(image_data: bytes):
-    url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
-
-    # Header otentikasi
-    headers = {
-        "pinata_api_key": API_KEY,
-        "pinata_secret_api_key": API_SECRET,
-    }
-
-    # Mengirim permintaan
-    files = {"file": ("image.png", image_data, "image/png")}
-    response = requests.post(url, files=files, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        print("File berhasil diunggah:")
-        print("IPFS Hash:", data["IpfsHash"])
-        print("Size:", data["PinSize"], "bytes")
-        return data
-    else:
-        print("Gagal mengunggah file:")
-        print(response.json())
-        return None
         
-def upload_public_file_to_pinata(file_path):
+def upload_public_file_to_pinata(file_path, filename):
     # Endpoint untuk mengunggah file ke Pinata
     url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
 
     # Membuka file yang ingin diunggah
     with open(file_path, "rb") as file:
-        files = {"file": file}
+        unique_name = f"Otti_{filename}"
+        
+        files = {
+            "file": file,
+            "pinataMetadata": (None, json.dumps({"name": unique_name})),
+            "pinataOptions": (None, json.dumps({"cidVersion": 1}))
+        }
+
 
         # Header otentikasi
         headers = {
-            "pinata_api_key": API_KEY,
-            "pinata_secret_api_key": API_SECRET,
+            "Authorization": f"Bearer {JWT_SECRET_KEY}"
         }
 
         # Mengirim permintaan POST ke Pinata
@@ -202,32 +220,13 @@ def upload_public_file_to_pinata(file_path):
         if response.status_code == 200:
             data = response.json()
             ipfs_hash = data["IpfsHash"]
-            public_url = f"https://gateway.pinata.cloud/ipfs/{ipfs_hash}"
-            print("File berhasil diunggah!")
-            print("IPFS Hash:", ipfs_hash)
-            print("URL Publik:", public_url)
-            return public_url
+            public_url = f"https://lime-absent-tarsier-87.mypinata.cloud/ipfs/{ipfs_hash}"
+            return ipfs_hash
         else:
             print("Gagal mengunggah file!")
             print(response.json())
             return None
 
-def get_private_file_from_pinata(ipfs_hash):
-    gateway_url = f"https://gateway.pinata.cloud/ipfs/{ipfs_hash}"
-
-    headers = {
-        "pinata_api_key": API_KEY,
-        "pinata_secret_api_key": API_SECRET,
-    }
-
-    response = requests.get(gateway_url, headers=headers)
-
-    if response.status_code == 200:
-        print("File berhasil diakses:")
-        print(response.content)  # Atau simpan file ini ke disk jika diperlukan
-    else:
-        print("Gagal mengakses file:")
-        print(response.json())
 
 def narray_to_bytes(narray: np.ndarray) -> bytes:
     img = Image.fromarray(narray)
